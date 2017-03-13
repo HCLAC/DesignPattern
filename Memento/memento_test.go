4a32ca8084de06a5c7980b636d2048e160583b12
HCLAC <hclac@sina.com>
2017-03-13 10:51:56 +0800

    Create memento_test.go

---
+++ Memento/memento_test.go
@@ -0,0 +1,18 @@
+package memento
+
+import (
+	"testing"
+)
+
+func TestMemento(t *testing.T) {
+	gr := GameRole{}
+	gr.GetInitState()
+	gr.StateDisplay()
+
+	caretaker := RoleStateCaretaker{}
+	caretaker.memento = gr.SaveState()
+	gr.Fight()
+	gr.StateDisplay()
+	gr.RecoveryState(caretaker.memento)
+	gr.StateDisplay()
+}memento_test.go
