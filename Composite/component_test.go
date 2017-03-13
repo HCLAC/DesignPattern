b3e9c35ca72ad8a23ecdbcdc5a23f604b153f14b
HCLAC <hclac@sina.com>
2017-03-13 09:49:15 +0800

    Create component_test.go

---
+++ Composite/component_test.go
@@ -0,0 +1,33 @@
+package component
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestComponent(t *testing.T) {
+	root := NewConcreateCompany("北京总公司")
+	root.add(NewHRDepartment("总公司人力资源部"))
+	root.add(NewFinanceDepartment("总公司财务部"))
+
+	compA := NewConcreateCompany("上海华东分公司")
+	compA.add(NewHRDepartment("上海华东分公司人力资源部"))
+	compA.add(NewFinanceDepartment("上海华东分公司财务部"))
+	root.add(compA)
+
+	compB := NewConcreateCompany("南京办事处")
+	compB.add(NewHRDepartment("南京办事处人力资源部"))
+	compB.add(NewFinanceDepartment("南京办事处财务部"))
+	compA.add(compB)
+
+	compC := NewConcreateCompany("杭州办事处")
+	compC.add(NewHRDepartment("杭州办事处人力资源部"))
+	compC.add(NewFinanceDepartment("杭州办事处财务部"))
+	compA.add(compC)
+
+	fmt.Println("结构体：")
+	root.display(1)
+
+	fmt.Println("职责：")
+	root.lineOfDuty()
+}
