7d98e77ae758b9aac525f9eb205084ee5cb7c9ca
HCLAC <hclac@sina.com>
2017-03-13 10:42:55 +0800

    Create interpreter.go

---
+++ Interpreter/interpreter.go
@@ -0,0 +1,46 @@
+/*
+ Interpreter 解释器模式：
+        给定一个语言，定义它的文法的一种表示，
+		并定义一个解释器，这个解释器使用该表示来解释语言中的句子
+ 个人想法：
+ 作者：   HCLAC
+*/
+
+package interpreter
+
+import (
+	"fmt"
+)
+
+type Context struct {
+	text string
+}
+
+// 抽象表达式
+type IAbstractExpression interface {
+	Interpret(*Context)
+}
+
+// 终结符表达式
+type TerminalExpression struct {
+}
+
+func (t *TerminalExpression) Interpret(context *Context) {
+	if t == nil {
+		return
+	}
+	context.text = context.text[:len(context.text)-1]
+	fmt.Println(context)
+}
+
+// 非终结符表达式
+type NonterminalExpression struct {
+}
+
+func (t *NonterminalExpression) Interpret(context *Context) {
+	if t == nil {
+		return
+	}
+	context.text = context.text[:len(context.text)-1]
+	fmt.Println(context)
+}
